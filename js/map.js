b03c57e9ae5d5f4fa6c51209eabd6cd8c3b4286c
NinetyNEIN <ninetynein@users.noreply.github.com>
2020-12-22 15:53:50 -0800

    Uploading test website
    Final project for good old BCIT

---
+++ js/map.js
@@ -0,0 +1,21 @@
+/*
+	Author: Tyson Haverkort
+	Date: June 15, 2015
+	Class: Comp1850 
+	Due: June 23, 2015. 
+	Description: This javascript file uses the leaflet library to load source data from
+				OpenStreetMaps, and overlay some points (current location of the boat)
+				and polylines (routes of trips)
+*/
+	
+/*Initialize the map and set the view center and zoom level*/
+var map = L.map('map').setView([49.276040, -123.14], 16);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// add a marker in the given location, attach some popup content to it and open the popup
+L.marker([49.275880, -123.139200]).addTo(map)
+    .bindPopup('Our Boat!')
+    .openPopup();
