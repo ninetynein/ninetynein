b9d90d8e92c18f6f38a95d36659ffaf8e59d884f
NinetyNEIN <ninetynein@users.noreply.github.com>
2020-12-23 20:59:59 -0800

    testing CORS issues

--- js/map.js
+++ js/map.js
@@ -1,21 +1,16 @@
 /*
 	Author: Tyson Haverkort
-	Date: June 15, 2015
-	Class: Comp1850 
-	Due: June 23, 2015. 
-	Description: This javascript file uses the leaflet library to load source data from
-				OpenStreetMaps, and overlay some points (current location of the boat)
-				and polylines (routes of trips)
+Testing storymap.js from KnightLab
 */
 	
 /*Initialize the map and set the view center and zoom level*/
-var map = L.map('map').setView([49.276040, -123.14], 16);
+// storymap_data can be an URL or a Javascript object
+var storymap_data = './storymaps/demo.json';
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// certain settings must be passed within a separate options object
+var storymap_options = {};
 
-// add a marker in the given location, attach some popup content to it and open the popup
-L.marker([49.275880, -123.139200]).addTo(map)
-    .bindPopup('Our Boat!')
-    .openPopup();
+var storymap = new VCO.StoryMap('map', storymap_data, storymap_options);
+window.onresize = function(event) {
+    storymap.updateDisplay(); // this isn't automatic
+}
